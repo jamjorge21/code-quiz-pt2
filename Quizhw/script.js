86d6d9341e260692b3ffd7bdbe974e69b9899abf
Jorge Martinez <70128888+jamjorge21@users.noreply.github.com>
2021-01-22 18:42:08 -0800

    Add files via upload

---
+++ Quizhw/script.js
@@ -0,0 +1,119 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What does CSS stand for?',
+    answers: [
+      { text: 'Cascading Style Sheets', correct: true },
+      { text: 'Cascading Super Shot', correct: false },
+      { text: 'Cascading Sexy Sheets', correct: false },
+      { text: 'Cascading Supa Sheet', correct: false }
+
+
+    ]
+  },
+  {
+    question: 'What does HTML stand for?',
+    answers: [
+      { text: 'Hypertool Markdown Language', correct: false },
+      { text: 'Hito Mark Lasagana', correct: false },
+      { text: 'Hypertext Markup Language', correct: true },
+      { text: 'Hypertext Mark Language', correct: false }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is computer science?',
+    answers: [
+      { text: 'Computer Science is the study of animals', correct: false },
+      { text: 'Computer Science is the study of oceacs', correct: false },
+      { text: 'Computer Science is the study of geography', correct: false },
+      { text: 'Computer Science is the study of computers and computational systems', correct: true }
+
+    ]
+  }
+]
